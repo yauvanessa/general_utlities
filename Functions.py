1c7c3e4634d4d6523dfed8c2e2577d129a542d78
Vanessa <vy2000@student.ubc.ca>
2023-06-06 14:22:03 -0700

    updated read_node_library function

--- Functions.py
+++ Functions.py
@@ -4,28 +4,32 @@ from os import listdir
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
-def replace_locations(sessionID:int, RXfile:str, TXfile:str, dir:str):
-    """function for replacing reciever GPS with more accurate location
+def replace_locations(sessionID:int, RXfile:str, TXfile:str, dir:str, datInfo=[]):
+    """function for replacing reciever GPS with more accurate location; loops through multiple nodes
 
     Args:
         sessionID (int): data ID number
         RXfile (str): RX file
         TXfile (str): TX file
-        dir (str): data directory to raw data
+        dir (str): data directory to raw data folders
+        datInfo (list): list of information (Unit ID, MEM number, Relay state, if current of potential)
     """
-    # load Rx file from csv
-    rxfile = pd.read_csv(os.path.join(dir, RXfile))
-    rxfile = rxfile.to_numpy()
-    rx_utm = rxfile[:,[2,3]]
-    # fit nearest neighbour to rx pts
-    nbrs_rx = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(rx_utm)
-
-    # load Tx file from csv
-    txfile = pd.read_csv(os.path.join(dir, TXfile))
-    txfile = txfile.to_numpy()
-    tx_utm = txfile[:,[2,3]] # TODO option to take from rx or tx file? or check which is closer and take that
-    # fit nearest neighbour to rx pts
-    nbrs_tx = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(tx_utm)
+    # select location from Rx OR Tx file
+    if len(datInfo) != 0 and datInfo[3] == "V":
+        # load Tx file from csv
+        txfile = pd.read_csv(os.path.join(dir, TXfile))
+        file = txfile.to_numpy()
+        tx_utm = file[:,[2,3]]
+        # fit nearest neighbour to tx pts
+        nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(tx_utm)
+    else: 
+        # use Rx file ("C"), even if None
+        # load Rx file from csv
+        rxfile = pd.read_csv(os.path.join(dir, RXfile))
+        file = rxfile.to_numpy()
+        rx_utm = file[:,[2,3]]
+        # fit nearest neighbour to rx pts
+        nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(rx_utm)     
 
     # load in data file gps pts
     all_folds = [x[0] for x in os.walk(dir)]
@@ -40,37 +44,36 @@ def replace_locations(sessionID:int, RXfile:str, TXfile:str, dir:str):
                 linesFIn = fIn.readlines()
                 fIn.close()
 
-                time, data = read_data(linesFIn)
+                # time, data = read_data(linesFIn)
                 gps = get_average_gps(linesFIn)
-                
+
                 # convert gps to UTM
                 gps_UTM = utm.from_latlon(gps[0],gps[1])
                 gps_UTM_pt = np.array([gps_UTM[0],gps_UTM[1]])
 
                 # find closest neighbour to gps pt
-                dists, inds = nbrs_rx.kneighbors([gps_UTM_pt])
+                dists, inds = nbrs.kneighbors([gps_UTM_pt])
 
                 # get closest gps pt from rx file
-                closest = rxfile[inds[0,0]]
+                closest = file[inds[0,0]]
 
                 # check accuracy
                 # print(f'diff1= ',gps_UTM_pt[0]-closest[2],'diff2= ',gps_UTM_pt[1]-closest[3])
 
-                # convert closest pt back to lat lon # TODO check error on this, maybe stick to UTM
-                closest_latlon = utm.to_latlon(closest[2],closest[3],gps_UTM[2],gps_UTM[3])
+                # convert closest pt back to lat lon # TODO check error on this, maybe stick to UTM -> yes.
+                # closest_latlon = utm.to_latlon(closest[2],closest[3],gps_UTM[2],gps_UTM[3])
 
-                # force new location to file
-                force_file(node,closest_latlon[0],closest_latlon[1],closest[4],False,1)
+                # force new location to file (in UTM)
+                force_file(node,closest[0],closest[1],closest[4],False,1) # TODO placeholder values
 
 
-
-def force_file(path:str, latitude:float, longitude:float, elevation:float, flag_new:bool, sid: int) -> None:
+def force_file(path:str, easting:float, northing:float, elevation:float, flag_new:bool, sid: int) -> None:
     """forces node files to a specified easting, northing, elevation
 
     Args:
         path (str): raw directory ID
-        latitude (float): known latitude
-        longitude (float): known longitude
+        easting (float): known latitude
+        northing (float): known longitude
         elevation (float): elevation
         flag_new (bool): _description_
         sid (int): session bucket ID
