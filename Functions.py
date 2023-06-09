8fb2469cb064b426269fde102e4163f8d871697c
Vanessa <vy2000@student.ubc.ca>
2023-06-09 14:25:06 -0700

    added try catch to functions

--- Functions.py
+++ Functions.py
@@ -17,57 +17,65 @@ def replace_locations(sessionID:int, RXfile:str, TXfile:str, dir:str, datInfo=[]
         dir (str): data directory to raw data folders
         datInfo (list): list of information (Unit ID, MEM number, Relay state, if current of potential)
     """
-    # select location from Rx OR Tx file
-    if len(datInfo) != 0 and datInfo[3] == "V":
-        # load Tx file from csv
-        txfile = pd.read_csv(os.path.join(dir, TXfile))
-        file = txfile.to_numpy()
-        tx_utm = file[:,[2,3]]
-        # fit nearest neighbour to tx pts
-        nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(tx_utm)
+    try: 
+        # select location from Rx OR Tx file
+        if len(datInfo) != 0 and datInfo[3] == "V":
+            # load Tx file from csv
+            txfile = pd.read_csv(os.path.join(dir, TXfile))
+            file = txfile.to_numpy()
+            tx_utm = file[:,[2,3]]
+            # fit nearest neighbour to tx pts
+            nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(tx_utm)
+        else: 
+            # use Rx file ("C"), even if None
+            # load Rx file from csv
+            rxfile = pd.read_csv(os.path.join(dir, RXfile))
+            file = rxfile.to_numpy()
+            rx_utm = file[:,[2,3]]
+            # fit nearest neighbour to rx pts
+            nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(rx_utm)
+    except:
+        print('Unable to load in rx or tx file')
+
     else: 
-        # use Rx file ("C"), even if None
-        # load Rx file from csv
-        rxfile = pd.read_csv(os.path.join(dir, RXfile))
-        file = rxfile.to_numpy()
-        rx_utm = file[:,[2,3]]
-        # fit nearest neighbour to rx pts
-        nbrs = NearestNeighbors(n_neighbors=1,algorithm='ball_tree',p=2).fit(rx_utm)     
 
-    # load in data file gps pts
-    all_folds = [x[0] for x in os.walk(dir)]
-    dat_folds = all_folds[1:]
+        try:
+            # load in data file gps pts
+            all_folds = [x[0] for x in os.walk(dir)]
+            dat_folds = all_folds[1:]
 
-    for m in range(len(dat_folds)):
-        # get files within data folder
-        only_files = [f for f in listdir(dat_folds[m])]
-        for idx in range(len(only_files)):
-                node = os.path.join(dat_folds[m], '', only_files[idx])
-                fIn = open(node, 'r', encoding="utf8", errors=""'ignore')
-                linesFIn = fIn.readlines()
-                fIn.close()
+            for m in range(len(dat_folds)):
+                # get files within data folder
+                only_files = [f for f in listdir(dat_folds[m])]
+                for idx in range(len(only_files)):
+                        node = os.path.join(dat_folds[m], '', only_files[idx])
+                        fIn = open(node, 'r', encoding="utf8", errors=""'ignore')
+                        linesFIn = fIn.readlines()
+                        fIn.close()
 
-                # time, data = read_data(linesFIn)
-                gps = get_average_gps(linesFIn)
+                        # time, data = read_data(linesFIn)
+                        gps = get_average_gps(linesFIn)
 
-                # convert gps to UTM
-                gps_UTM = utm.from_latlon(gps[0],gps[1])
-                gps_UTM_pt = np.array([gps_UTM[0],gps_UTM[1]])
+                        # convert gps to UTM
+                        gps_UTM = utm.from_latlon(gps[0],gps[1])
+                        gps_UTM_pt = np.array([gps_UTM[0],gps_UTM[1]])
 
-                # find closest neighbour to gps pt
-                dists, inds = nbrs.kneighbors([gps_UTM_pt])
+                        # find closest neighbour to gps pt
+                        dists, inds = nbrs.kneighbors([gps_UTM_pt])
 
-                # get closest gps pt from rx file
-                closest = file[inds[0,0]]
+                        # get closest gps pt from rx file
+                        closest = file[inds[0,0]]
 
-                # check accuracy
-                # print(f'diff1= ',gps_UTM_pt[0]-closest[2],'diff2= ',gps_UTM_pt[1]-closest[3])
+                        # check accuracy
+                        # print(f'diff1= ',gps_UTM_pt[0]-closest[2],'diff2= ',gps_UTM_pt[1]-closest[3])
 
-                # convert closest pt back to lat lon # TODO check error on this, maybe stick to UTM -> yes.
-                # closest_latlon = utm.to_latlon(closest[2],closest[3],gps_UTM[2],gps_UTM[3])
+                        # convert closest pt back to lat lon # TODO check error on this, maybe stick to UTM -> yes.
+                        # closest_latlon = utm.to_latlon(closest[2],closest[3],gps_UTM[2],gps_UTM[3])
 
-                # force new location to file (in UTM)
-                force_file(node,closest[0],closest[1],closest[4],False,1) # TODO placeholder values
+                        # force new location to file (in UTM)
+                        force_file(node,closest[0],closest[1],closest[4],False,1) # TODO placeholder values
+        except:
+            print('Unable to replace GPS location with rx or tx location')
 
 
 def force_file(path:str, easting:float, northing:float, elevation:float, flag_new:bool, sid: int) -> None:
@@ -90,29 +98,41 @@ def polarity_correction(dir:str):
         dir (str): data file (ascii)
     """
      
-     # read in ASCII file
-    fIn = open(dir, 'r', encoding="utf8", errors='ignore')
-    linesFIn = fIn.readlines()
-    print(fIn.read())
-    fIn.close()
+    try:
+        # read in ASCII file
+        fIn = open(dir, 'r', encoding="utf8", errors='ignore')
+        linesFIn = fIn.readlines()
+        fIn.close()
+        
+    except: 
+        print('Unable to load in data file')
+    
+    else:
+        try:
+            # get conversion factor from header
+            cfstr = 'Conversion Factor'
+            for n in range(0,17): # number of header option; takes too long to check all
+                teststr = linesFIn[n]
 
-    # get conversion factor from header
-    cfstr = 'Conversion Factor'
-    for n in range(0,17): # number of header option; takes too long to check all
-        teststr = linesFIn[n]
+                if cfstr in teststr:
+                    cf = linesFIn[n]
+                    addto = cf.split(':')
 
-        if cfstr in teststr:
-            cf = linesFIn[n]
-            addto = cf.split(':')
-            cfnew = addto[0] + ':-' + addto[1]
-            linesFIn[n] = cfnew # rewrite correction
+                    # check if already negative
+                    if '-' in addto[1]:
+                        raise Exception('Polarity of conversion factor is already negative')
+                    else:
+                        cfnew = addto[0] + ':-' + addto[1]
+                        linesFIn[n] = cfnew # rewrite correction
 
-            # overwrite file with fix
-            fOut = open(dir, 'w')
-            for f in linesFIn:
-                fOut.write('%s' % f)
-            fOut.close()
-
+                        # overwrite file with fix
+                        fOut = open(dir, 'w')
+                        for f in linesFIn:
+                            fOut.write('%s' % f)
+                        fOut.close()
+        except:
+            print('Unable to change conversion factor polarity, may already be neg')
+            
 
 def update_resistivity(fIn:str):
     """loads the h5file and calculates + updates the resistivity; rewrites the h5file
@@ -121,26 +141,42 @@ def update_resistivity(fIn:str):
     Args:
         fIn (str): hdf5 file
     """
-    # read in h5 file
-    f = h5py.File(fIn, 'r+')
+    try:
+        # read in h5 file
+        f = h5py.File(fIn, 'r+')
+    except:
+        print('Could not properly read in file, check file formatting')
 
-    # assign variables from file
-    V = f.attrs['voltage']
-    I = f.attrs['current']
-    #K = f.attrs['geometric_factor']
+    else: 
+        # assign variables from file
+        V = f.attrs['voltage']
+        I = f.attrs['current']
+        #K = f.attrs['geometric_factor']
 
-    # update geometric factor K
-    K = calculate_geofactor(f.attrs['receiver1_easting'], f.attrs['receiver1_northing'], f.attrs['receiver1_altitude'], 
-                            f.attrs['receiver2_easting'], f.attrs['receiver2_northing'], f.attrs['receiver2_altitude'],
-                            f.attrs['transmit_easting'], f.attrs['transmit_northing'], f.attrs['transmit_altitude'],
-                            f.attrs['transmit2_easting'], f.attrs['transmit2_northing'], f.attrs['transmit2_altitude'])
+        try: 
+            # update geometric factor K
+            K = calculate_geofactor(f.attrs['receiver1_easting'], f.attrs['receiver1_northing'], f.attrs['receiver1_altitude'], 
+                                    f.attrs['receiver2_easting'], f.attrs['receiver2_northing'], f.attrs['receiver2_altitude'],
+                                    f.attrs['transmit_easting'], f.attrs['transmit_northing'], f.attrs['transmit_altitude'],
+                                    f.attrs['transmit2_easting'], f.attrs['transmit2_northing'], f.attrs['transmit2_altitude'])
 
-    # recalculate resistivity
-    res = 2*np.pi*K*(V/I)
+            # recalculate resistivity
+            res = 2*np.pi*K*(V/I)
+
+            if res <= 0:
+                raise Exception('Unrealistic resistivity')
+
+        except:
+            f.attrs['dc_qc'] = 'reject'
+            f.attrs['ip_qc'] = 'reject'
+            print('Resistivity calculation failed, dc_qc and ip_qc rejected')
+
+        else:
+            # assign new resistivity to file
+            f.attrs['apparent_resistivity'] = res
     
-    # assign new resistivity to file
-    f.attrs['apparent_resistivity'] = res
-    f.close()    
+        finally:
+            f.close()
 
 
 def calculate_geofactor(Rx1East,Rx1North,Rx1Elev,Rx2East,Rx2North,Rx2Elev,Tx1East,Tx1North,Tx1Elev,Tx2East,Tx2North,Tx2Elev):
@@ -181,38 +217,58 @@ def calculate_geofactor(Rx1East,Rx1North,Rx1Elev,Rx2East,Rx2North,Rx2Elev,Tx1Eas
     return K_new
 
 
-def decay_fit(fIn:str):
+def stretched_exponential_decay_fit(fIn:str):
     """fits decay curve to a stretched exponential function; returns fit parameters
 
     Args:
         fIn (str): hdf5 file
     """
+    try: 
+        # read in h5 file
+        f = h5py.File(fIn, 'r+')
 
-    # read in h5 file
-    f = h5py.File(fIn, 'r+')
+        t = f['vs_window_centers']
+        t = t[:]/1000 # get from ms to s
+        Vs = f['decay']
+        # plt.scatter(t,Vs)
+    except:
+        print('Could not properly read in file or extract values, check file formatting')
 
-    t = f['vs_window_centers']
-    t = t[:]/1000 # get from ms to s
-    Vs = f['decay']
-    # plt.scatter(t,Vs)
+    else:
+        try:
+            # fit decay curve
+            popt, _ = curve_fit(exponential_decay_estimate, t, Vs, p0=(0.5,0.5,0.5)) # random initial guesses req to get ok fit
+            a, b, c = popt # get fit parameters
 
-    # fit decay curve
-    popt, _ = curve_fit(exponential_decay_estimate, t, Vs, p0=(0.5,0.5,0.5)) # random initial guesses req to get ok fit
-    a, b, c = popt # get fit parameters
+            if 1 in popt: # a, b, c will == 1 if it didn't work
+                f.attrs['ip_qc'] = 'reject' 
+                raise Exception('Decay fit could not accurately fit data')
+        
+        except:
+            print('Curve_fit function could not fit data')
+            
+        else:
+            try:
+                # get y data from fit
+                exp_fit = exponential_decay_estimate(t, a, b, c)
+                # plt.scatter(t,exp_fit)
 
-    # get y data from fit
-    exp_fit = exponential_decay_estimate(t, a, b, c)
-    # plt.scatter(t,exp_fit)
+                # calculate std of fit
+                std = np.std(Vs - exp_fit)
 
-    # calculate std of fit
-    std = np.std(Vs - exp_fit)
+                if std > 0.1: # TODO is this too high/low?
+                    f.attrs['ip_qc'] = 'reject' 
+                else:
+                    # write parameters to file
+                    f.attrs['decay_fit_a'] = a
+                    f.attrs['decay_fit_b'] = b
+                    f.attrs['decay_fit_c'] = c
+                    f.attrs['decay_fit_std'] = std
+            except:
+                print('Decay function produced an intangible value')
 
-    # write parameters to file
-    f.attrs['decay_fit_a'] = a
-    f.attrs['decay_fit_b'] = b
-    f.attrs['decay_fit_c'] = c
-    f.attrs['decay_fit_std'] = std
-    f.close()
+        finally:
+            f.close()
 
 
 def exponential_decay_estimate(x, a, b, c):
